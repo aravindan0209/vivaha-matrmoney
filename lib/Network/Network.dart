02763888b92c7a6c038a80e95acf5e8852ade249
vigne <Ahbus2823*>
2022-02-02 12:34:12 +0530

    Adding base API function and Login screen

--- lib/Network/Network.dart
+++ lib/Network/Network.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 
 class Network {
-  final String _url = 'https://lankavivaha.com/dev/1';
+  final String _url = 'https://lankavivaha.com/dev/';
 
   login(apiUrl, data) async {
     return await Dio().post(apiUrl,
